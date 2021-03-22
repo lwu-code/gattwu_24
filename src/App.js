2253b4237e4699339f02cfd8f0e0e1cb2fd85808
Li Wu <lwu@litepoint.internal>
2021-03-21 18:37:32 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import logo from './logo.svg';
+
+import React from "react"
 import './App.css';
 import Poker from "./components/poker"
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Poker />
-          Hello Garrett and Friends
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends React.Component {
+
+  // var x1 = Math.floor((Math.random() * 10) + 1);
+  // // document.getElementById("num1").value = x1.toString();
+  // var x2 = Math.floor((Math.random() * 10) + 1);
+  // // document.getElementById("num2").value = x2.toString();
+  // var x3 = Math.floor((Math.random() * 10) + 1);
+  // // document.getElementById("num3").value = x3.toString();
+  // var x4 = Math.floor((Math.random() * 10) + 1);
+  // // document.getElementById("num4").value = x4.toString();
+
+  constructor(props) {
+    super();
+    var x1 = Math.floor((Math.random() * 10) + 1);
+  // document.getElementById("num1").value = x1.toString();
+  var x2 = Math.floor((Math.random() * 10) + 1);
+  // document.getElementById("num2").value = x2.toString();
+  var x3 = Math.floor((Math.random() * 10) + 1);
+  // document.getElementById("num3").value = x3.toString();
+  var x4 = Math.floor((Math.random() * 10) + 1);
+  // document.getElementById("num4").value = x4.toString();
+    // Don't call this.setState() here!
+    this.state = {
+      card1: x1.toString(),
+      card2: x2.toString(),
+      card3: x3.toString(),
+      card4: x4.toString()
+
+    };
+    // this.handleClick = this.handleClick.bind(this);
+
+
+
+
+  }
+  render(){
+    return (
+
+      <div className="App">
+        <header className="App-header">
+  
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Poker dataFromParent = {this.state.card1 + ":" + this.state.card2 + ":" + this.state.card3 + ":" + this.state.card4}/>
+            Hello Garrett and Friends 2
+          </a>
+        </header>
+        <img src={'/images/cards/2C.png'}  />
+      </div>
+    );
+  }
+
 }
 
 export default App;
--- src/components/poker.js
+++ src/components/poker.js
@@ -1,10 +1,15 @@
 import React from "react"
+import Card2C from "../images/cards/2C.png"
+
+
+
 class Poker extends React.Component {
   render() {
     return (
       <div>
-        <h1>Hello from Create React App</h1>
+        <h1>Hello {this.props.dataFromParent} </h1>
         <p>I am in a React Component!</p>
+        <img src={Card2C}  />
       </div>
     )
   }
