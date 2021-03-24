77582301a6db65dd93118903557104712472fd12
Li Wu <lwu@litepoint.internal>
2021-03-24 16:24:08 -0700

    update

--- src/App.js
+++ src/App.js
@@ -6,31 +6,13 @@ import Poker from "./components/poker"
 
 class App extends React.Component {
 
-  // var x1 = Math.floor((Math.random() * 10) + 1);
-  // // document.getElementById("num1").value = x1.toString();
-  // var x2 = Math.floor((Math.random() * 10) + 1);
-  // // document.getElementById("num2").value = x2.toString();
-  // var x3 = Math.floor((Math.random() * 10) + 1);
-  // // document.getElementById("num3").value = x3.toString();
-  // var x4 = Math.floor((Math.random() * 10) + 1);
-  // // document.getElementById("num4").value = x4.toString();
+
 
   constructor(props) {
     super();
-    var x1 = Math.floor((Math.random() * 10) + 1);
-  // document.getElementById("num1").value = x1.toString();
-  var x2 = Math.floor((Math.random() * 10) + 1);
-  // document.getElementById("num2").value = x2.toString();
-  var x3 = Math.floor((Math.random() * 10) + 1);
-  // document.getElementById("num3").value = x3.toString();
-  var x4 = Math.floor((Math.random() * 10) + 1);
-  // document.getElementById("num4").value = x4.toString();
     // Don't call this.setState() here!
     this.state = {
-      card1: x1.toString(),
-      card2: x2.toString(),
-      card3: x3.toString(),
-      card4: x4.toString()
+
 
     };
     // this.handleClick = this.handleClick.bind(this);
@@ -54,7 +36,7 @@ class App extends React.Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <Poker dataFromParent = {this.state.card1 + ":" + this.state.card2 + ":" + this.state.card3 + ":" + this.state.card4}/>
+            <Poker />
             Hello Garrett and Friends 2
           </a>
         </header>
