e4a56e7f58d521d4a6067131414f71c703cb8795
Li Wu <lwu@litepoint.internal>
2021-05-02 13:30:33 -0700

    new code for AI

--- src/components/AI.js
+++ src/components/AI.js
@@ -1,171 +1,109 @@
-import React, { useEffect, useState, useRef } from "react";
-// import logo from "./logo.svg";
-import {
-  TextField,
-  Grid,
-  AppBar,
-  Toolbar,
-  Typography,
-  Box,
-  IconButton,
-  Button,
-} from "@material-ui/core";
-import "../App.css";
-import MenuIcon from "@material-ui/icons/Menu";
-import { makeStyles } from "@material-ui/core/styles";
+import React from "react";
+import ReactDOM from "react-dom";
+
 import * as cocoSsd from "@tensorflow-models/coco-ssd";
-import * as tf from "@tensorflow/tfjs";
-// import {loadGraphModel} from '@tensorflow/tfjs-converter';
+import "@tensorflow/tfjs";
+import "../App.css";
 
-// import * as posenet from '@tensorflow-models/posenet';
-import Webcam from "react-webcam";
-// import { createWorker, createScheduler } from "tesseract.js";
-// import * as cvstfjs from '@microsoft/customvision-tfjs';
+class AI extends React.Component {
+  videoRef = React.createRef();
+  canvasRef = React.createRef();
 
-var video_width = 960;
-var video_height = 580;
-var top_distance = "100px";
-
-function AI() {
-  const useStyles = makeStyles((theme) => ({
-    root: {
-      flexGrow: 1,
-    },
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
-    title: {
-      flexGrow: 1,
-    },
-  }));
-  const classes = useStyles();
-
-  const webcamRef = React.useRef(null);
-
-  const [videoWidth, setVideoWidth] = useState(video_width);
-  const [videoHeight, setVideoHeight] = useState(video_height);
-
-  const [model, setModel] = useState();
-
-  async function loadModel() {
-    try {
-      const model = await cocoSsd.load();
-      setModel(model);
-      console.log("setloadedModel");
-    } catch (err) {
-      console.log(err);
-      console.log("failed load model");
+  componentDidMount() {
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      const webCamPromise = navigator.mediaDevices
+        .getUserMedia({
+          audio: false,
+          video: {
+            facingMode: "user",
+          },
+        })
+        .then((stream) => {
+          window.stream = stream;
+          this.videoRef.current.srcObject = stream;
+          return new Promise((resolve, reject) => {
+            this.videoRef.current.onloadedmetadata = () => {
+              resolve();
+            };
+          });
+        });
+      const modelPromise = cocoSsd.load();
+      Promise.all([modelPromise, webCamPromise])
+        .then((values) => {
+          this.detectFrame(this.videoRef.current, values[0]);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
     }
   }
 
-  useEffect(() => {
-    tf.ready().then(() => {
-      loadModel();
+  detectFrame = (video, model) => {
+    model.detect(video).then((predictions) => {
+      this.renderPredictions(predictions);
+      requestAnimationFrame(() => {
+        this.detectFrame(video, model);
+      });
     });
-  }, []);
-
-  async function predictionFunction() {
-    const predictions = await model.detect(document.getElementById("img"));
-    setVideoHeight(webcamRef.current.video.videoHeight);
-    setVideoWidth(webcamRef.current.video.videoWidth);
-    var cnvs = document.getElementById("myCanvas");
-
-    cnvs.style.position = "absolute";
-
-    var ctx = cnvs.getContext("2d");
-    ctx.clearRect(
-      0,
-      0,
-      webcamRef.current.video.videoWidth,
-      webcamRef.current.video.videoHeight
-    );
-
-    if (predictions.length > 0) {
-      // setPredictionData(predictions);
-      console.log(predictions);
-      for (let n = 0; n < predictions.length; n++) {
-        // Check scores
-        console.log(n);
-        if (predictions[n].score > 0.8) {
-          let bboxLeft = predictions[n].bbox[0];
-          let bboxTop = predictions[n].bbox[1];
-          let bboxWidth = predictions[n].bbox[2];
-          let bboxHeight = predictions[n].bbox[3];
-
-          console.log("bboxLeft: " + bboxLeft);
-          console.log("bboxTop: " + bboxTop);
-
-          console.log("bboxWidth: " + bboxWidth);
-
-          console.log("bboxHeight: " + bboxHeight);
-
-          ctx.beginPath();
-          ctx.font = "28px Arial";
-          ctx.fillStyle = "red";
-
-          ctx.fillText(
-            predictions[n].class +
-              ": " +
-              Math.round(parseFloat(predictions[n].score) * 100) +
-              "%",
-            bboxLeft,
-            bboxTop
-          );
-
-          ctx.rect(bboxLeft + 200, bboxTop, bboxWidth, bboxHeight);
-          ctx.strokeStyle = "#FF0000";
-
-          ctx.lineWidth = 3;
-          ctx.stroke();
-
-          console.log("detected");
-        }
-      }
-    }
-
-    setTimeout(() => predictionFunction(), 500);
-  }
-
-  const videoConstraints = {
-    height: video_height,
-    width: video_width,
-    maxWidth: "100vw",
-    facingMode: "environment",
   };
 
-  return (
-    <div className="App">
-      <div style={{ position: "absolute", top: top_distance, zIndex: "9999" }}>
-        <canvas
-          id="myCanvas"
-          width={videoWidth}
-          height={videoHeight}
-          style={{ backgroundColor: "transparent" }}
-        />
-      </div>
-      {/* <div style={{ position: "absolute", top: "400px" }}> */}
-      <header className="App-header">
-        <Button
-          onClick={() => {
-            predictionFunction();
-          }}
-        >
-          Click me to start detection
-        </Button>
+  renderPredictions = (predictions) => {
+    const ctx = this.canvasRef.current.getContext("2d");
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    // Font options.
+    const font = "16px sans-serif";
+    ctx.font = font;
+    ctx.textBaseline = "top";
+    predictions.forEach((prediction) => {
+      const x = prediction.bbox[0];
+      const y = prediction.bbox[1];
+      const width = prediction.bbox[2];
+      const height = prediction.bbox[3];
+      // Draw the bounding box.
+      ctx.strokeStyle = "#00FFFF";
+      ctx.lineWidth = 4;
+      ctx.strokeRect(x, y, width, height);
+      // Draw the label background.
+      ctx.fillStyle = "#00FFFF";
+      const textWidth = ctx.measureText(prediction.class).width;
+      const textHeight = parseInt(font, 10); // base 10
+      ctx.fillRect(x, y, textWidth + 4, textHeight + 4);
+    });
 
-        <Webcam
-          audio={false}
-          id="img"
-          ref={webcamRef}
-          // width={640}
-          screenshotQuality={1}
-          screenshotFormat="image/jpeg"
-          videoConstraints={videoConstraints}
-        />
-        {/* </div> */}
-      </header>
-    </div>
-  );
+    predictions.forEach((prediction) => {
+      const x = prediction.bbox[0];
+      const y = prediction.bbox[1];
+      // Draw the text last to ensure it's on top.
+      ctx.fillStyle = "#000000";
+      ctx.fillText(prediction.class, x, y);
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <div>
+          <video
+            className="size"
+            autoPlay
+            playsInline
+            muted
+            ref={this.videoRef}
+            width="600"
+            height="500"
+          />
+          <canvas
+            className="size"
+            ref={this.canvasRef}
+            width="600"
+            height="500"
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
+const rootElement = document.getElementById("root");
+ReactDOM.render(<AI />, rootElement);
 export default AI;
